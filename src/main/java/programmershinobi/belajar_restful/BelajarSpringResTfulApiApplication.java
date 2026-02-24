3054ef607303364bb58a7742a1659ac05525dfc4
programmerShinobi <shinobiprogrammer@gmail.com>
2026-02-24 09:26:37 +0700

    [add] setup project

---
+++ src/main/java/programmershinobi/belajar_restful/BelajarSpringResTfulApiApplication.java
@@ -0,0 +1,13 @@
+package programmershinobi.belajar_restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringResTfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringResTfulApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/programmershinobi/belajar_restful/BelajarSpringResTfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package programmershinobi.belajar_restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringResTfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
