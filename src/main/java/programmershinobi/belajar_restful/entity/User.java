b52ec41480f75a737e3b4adb47d3172186fab65b
programmerShinobi <shinobiprogrammer@gmail.com>
2026-02-24 10:37:37 +0700

    [add] learn : user entity

---
+++ src/main/java/programmershinobi/belajar_restful/entity/User.java
@@ -0,0 +1,34 @@
+package programmershinobi.belajar_restful.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    private String username;
+
+    private String password;
+
+    private String name;
+
+    private String token;
+
+    @Column(name = "token_expired_at")
+    private Long tokenExpiredAt;
+
+    @OneToMany(mappedBy = "user")
+    private List<Contact> contacts;
+}
